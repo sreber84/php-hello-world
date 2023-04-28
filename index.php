d6aab2b4752eaa8e3d497a8cd3101d498314025b
Simon Reber <sreber84@users.noreply.github.com>
2023-04-28 17:06:39 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+
+  <head>
+    <title>Hello World!</title>
+    <style>
+      h1 {
+        text-align: center;
+      }
+      
+      h2 {
+        text-align: left;
+      }
+      
+      h3 {
+        text-align: right;
+      }
+</style>
+  </head>
+
+  <body>
+    <?php echo "<h1> Hello World! </h1>"; ?>
+    
+    <?php phpinfo(); ?>
+    <?php error_log('This is a Test message'); ?>
+  </body>
+
+</html>
